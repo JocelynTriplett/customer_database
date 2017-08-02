c2c67114d5f73bd33cca7b10cf8fb14f4797a638
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-02 15:59:37 -0400

    more tweaking

--- main.js
+++ main.js
@@ -5,18 +5,24 @@ function personInfo(){
   for (var p = 0; p < people.length; p++) {
     let person= `
       <div class="person">
-      <div class="photo">
-      <img src=${people[p].picture.large}>
-      </div>
+        <div class="photo">
+          <img src=${people[p].picture.large}>
+        </div>
       <div class="information">
+      <div class="name">
       <h2>${people[p].name.first} ${people[p].name.last}</h2>
+      </div>
       <h3>${people[p].email}</h3>
+      <div class="addressSpacer"></div>
       <p>${people[p].location.street}</p>
       <p>${people[p].location.city}, ${people[p].location.state} ${people[p].location.postcode}</p>
       <p class="phone">${people[p].phone}</p>
+      <div class="addressSpacer"></div>
       <p class="SSN">${people[p].id.value}</p>
       </div>
+      <div class="endspacer"></div>
       </div>
+
     `
     gallery.innerHTML += person;
   }
