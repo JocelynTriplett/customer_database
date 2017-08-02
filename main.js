591a0749f19d1541532127be13dc43a61c46aaae
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-02 15:43:57 -0400

    mostly ok

---
+++ main.js
@@ -0,0 +1,29 @@
+
+console.log(customers);
+console.log(customers.results[0].picture.medium);
+
+let people = customers.results;
+
+function personInfo(){
+  let gallery = document.getElementById('gallery')
+  for (var p = 0; p < people.length; p++) {
+    let person= `
+      <div class="person">
+      <div class="photo">
+      <img src=${people[p].picture.large}>
+      </div>
+      <div class="information">
+      <h2>${people[p].name.first} ${people[p].name.last}</h2>
+      <h3>${people[p].email}</h3>
+      <p>${people[p].location.street}</p>
+      <p>${people[p].location.city}, ${people[p].location.state} ${people[p].location.postcode}</p>
+      <p class="phone">${people[p].phone}</p>
+      <p class="SSN">${people[p].id.value}</p>
+      </div>
+      </div>
+    `
+    gallery.innerHTML += person;
+  }
+}
+
+personInfo(people);
